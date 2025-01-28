0e098fafc2f1007d6d4dfe813743260f1fe379bd
SamuelRiveira <samuelriveiraescudero@gmail.com>
2025-01-28 13:56:55 +0000

    Añadir imagen y terminar diseño

--- src/main/kotlin/WelcomeScreen.kt
+++ src/main/kotlin/WelcomeScreen.kt
@@ -1,11 +1,14 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -16,11 +19,35 @@ fun WelcomeScreen(){
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.End,
-            modifier = Modifier.height(50.dp).fillMaxWidth().padding(end = 16.dp)
+            modifier = Modifier.height(70.dp).fillMaxWidth().padding(end = 16.dp)
         ) {
-            Text(text = "Desconectar")
+            Image(
+                painter = painterResource("perfil.png"),
+                contentDescription = "Perfil",
+                modifier = Modifier.padding(8.dp)
+            )
+            Column(
+
+            ) {
+                Row {
+                    Text(
+                        text = "Samu",
+                        fontSize = 14.sp
+                    )
+                    Text(
+                        text = " (Gestor)",
+                        fontSize = 14.sp
+                    )
+                }
+                Text(
+                    text = "Desconectar",
+                    fontSize = 14.sp,
+                    color = Color.Blue,
+                    modifier = Modifier.clickable{}
+                )
+            }
         }
         Divider(modifier = Modifier.padding(bottom = 24.dp))
-        Text("Siguiente contenido")
+
     }
 }
